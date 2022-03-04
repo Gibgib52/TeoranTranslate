372e78af16438ca7aeff35ab094efa8673bdee70
Gibson Hall <gibsonjhall@gmail.com>
2022-03-03 20:58:58 -0600

    framework for bot

--- TeoranTranslateBot.py
+++ TeoranTranslateBot.py
@@ -1,4 +1,32 @@
 """
     Translates english into "Teoran" from Gwain Saga through a discord bot.
+
+    used https://www.youtube.com/watch?v=bq80J5rh4Cc as template
 """
 
+import discord
+
+import json
+import os
+
+# token loading and config
+if os.path.exists(os.getcwd() + "/config.json"):
+    with open("./config.json") as f:
+        configData = json.load(f)
+
+else:
+    configTemplate = {"Token": "", "Prefix": "$"}
+
+    with open(os.getcwd() + "/config.json","w+") as f:
+        json.dump(configTemplate, f)
+
+token = configData["Token"]
+prefix = configData["Prefix"]
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Logged in as {}".format(client))
+
+client.run(token)
