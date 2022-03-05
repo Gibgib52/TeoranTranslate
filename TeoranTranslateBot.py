cf0a9dadff85131c6ef7fc1745f7e623d758c5bb
Gibson Hall <gibsonjhall@gmail.com>
2022-03-04 19:30:01 -0600

    Added numbers and some punctuation

--- TeoranTranslateBot.py
+++ TeoranTranslateBot.py
@@ -84,6 +84,7 @@ async def help(ctx):
 @bot.command()
 async def translate(ctx, string):
     translateDict = {
+        # letters
         "a": "<:Teoran_a:949162522148216882> ",
         "b": "<:Teoran_b:949162522198556692> ",
         "c": "<:Teoran_c:949162522336956436> ",
@@ -110,7 +111,25 @@ async def translate(ctx, string):
         "x": "<:Teoran_x:949162521854607381> ",
         "y": "<:Teoran_y:949162522169192488> ",
         "z": "<:Teoran_z:949162521955299349> ",
-        " ": "<:Teoran_space:949164879405780997> "
+        " ": "<:Teoran_space:949164879405780997> ",
+        # numbers
+        "1": "<:Teoran_1:949476249309425675> ",
+        "2": "<:Teoran_2:949476249208774736> ",
+        "3": "<:Teoran_3:949476248860643349> ",
+        "4": "<:Teoran_4:949476249225531392> ",
+        "5": "<:Teoran_5:949476249133256724> ",
+        "6": "<:Teoran_6:949476249275879454> ",
+        "7": "<:Teoran_7:949476249183612928> ",
+        "8": "<:Teoran_8:949476248898379827> ",
+        "9": "<:Teoran_9:949476248957095977> ",
+        "0": "<:Teoran_0:949476249301024818> ",
+        # punctuation
+        "?": "<:Teoran_question:949476249070366761> ",
+        ";": "<:Teoran_semicolon:949476249082937404> ",
+        ":": "<:Teoran_colon:949476249095528480> ",
+        "#": "<:Teoran_hash:949476248680284191> ",
+        "!": "<:Teoran_exclamation:949476248831291423> ",
+        ".": "<:Teoran_period:949476249166811216> "
     }
 
     preppedString = string.lower() # lower so capital letters dont suprise us
